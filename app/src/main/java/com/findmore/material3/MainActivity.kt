71b88e6cee7f2e5bb958f62665cc2f0281615573
shiva <shivayogi@tropogo.com>
2024-04-05 08:36:09 +0530

    TopAppBar, adding action btns, Nav Icon,Scaffold usage

--- app/src/main/java/com/findmore/material3/MainActivity.kt
+++ app/src/main/java/com/findmore/material3/MainActivity.kt
@@ -5,20 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.ui.Alignment
-import com.findmore.material3.ui.theme.Material3Theme
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import com.findmore.material3.ui.theme.Material3Theme
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,36 +32,60 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
 
-                        Button(onClick = { }) {
-                            Text(text = "Subscribe")
-                        }
-                        ElevatedButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Outlined.Add,
-                                contentDescription = "Add to cart",
-                                modifier = Modifier.size(18.dp)
+                    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
+                    // val scrollBehavior=TopAppBarDefaults.enterAlwaysScrollBehavior()
+                    //   val scrollBehavior=TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+                    Scaffold(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .nestedScroll(scrollBehavior.nestedScrollConnection),
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(text = "My Notes")
+
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.ArrowBack,
+                                            contentDescription = "Go Back"
+                                        )
+                                    }
+                                },
+                                actions = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.FavoriteBorder,
+                                            contentDescription = "Mark as favorite"
+                                        )
+                                    }
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Edit,
+                                            contentDescription = "Edit notes "
+                                        )
+                                    }
+                                },
+                                scrollBehavior = scrollBehavior
                             )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Text(text = "add to cart")
                         }
-                        FilledTonalButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Open in browser")
+                    ) { values ->
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(values)
+                        ) {
+                            items(100) {
+                                Text(
+                                    text = "Item$it",
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
                         }
-                        OutlinedButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Back")
-                        }
-                        TextButton(onClick = { /*TODO*/ }) {
-                            Text(text = "Learn More")
-                        }
-
-
                     }
+
                 }
             }
         }
